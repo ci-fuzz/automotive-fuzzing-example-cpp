262bdf7c158cad2d23fa515c5d8942307468d7f3
D. Teuchert <teuchert@code-intelligence.com>
2022-06-27 14:31:35 +0200

    changes to use new cli

--- .code-intelligence/build.sh
+++ .code-intelligence/build.sh
@@ -1,7 +1,5 @@
 set -e
-mkdir -p $WORK_DIR
-cd $WORK_DIR
-mkdir -p .cifuzz
-
-cmake -DCIFUZZ_INSTALL_ROOT=/opt/ci-fuzz-2.30.0 ..
+mkdir -p build
+cd build
+cmake .. # Here, you can place your CMake options such as -DDISABLE_TESTS
 make -j$(nproc)
