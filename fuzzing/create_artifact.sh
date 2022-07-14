40e8b8007e339d1ca7d7a7f7277518496f5c349c
D. Teuchert <teuchert@code-intelligence.com>
2022-07-14 11:36:24 +0200

    adjusted artifact creation script

--- fuzzing/create_artifact.sh
+++ fuzzing/create_artifact.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
-# Name of fuzz test
-FUZZ_TEST=$1
+for fuzz_test_file in *fuzz*.cpp
+do
 
-if test -f "$FUZZ_TEST.cpp"; then
+    FUZZ_TEST=${fuzz_test_file%.*}
     # Create temporary working directory
-    mkdir unpacking
+    mkdir -p unpacking
     # Create bundle with cifuzz (this will only contain the libfuzzer target and no llvm_cov target)
     cifuzz bundle $FUZZ_TEST -o unpacking/$FUZZ_TEST.tar.gz
+
+    # Build llvm_cov target
+    ./fuzzing/coverage_report.sh $FUZZ_TEST
 
     # Change directory in work dir and unpack artifact
     cd unpacking
@@ -16,7 +19,9 @@ if test -f "$FUZZ_TEST.cpp"; then
     rm $FUZZ_TEST.tar.gz
 
     # Add things to cifuzz.yaml
-    echo "      seeds: cifuzz-corpus/$FUZZ_TEST" >> cifuzz.yaml
+    if [ -d "cifuzz-corpus/$FUZZ_TEST" ]; then
+      echo "      seeds: cifuzz-corpus/$FUZZ_TEST" >> cifuzz.yaml
+    fi
     echo "      max_run_time: 1800" >> cifuzz.yaml
     echo "      parallel_executions: 1" >> cifuzz.yaml
     echo "    - target: $FUZZ_TEST" >> cifuzz.yaml
@@ -24,7 +29,9 @@ if test -f "$FUZZ_TEST.cpp"; then
     echo "      sanitizer: unknown_sanitizer" >> cifuzz.yaml
     echo "      build_dir: /home/dte/Documents/Intel/automotive-fuzzing-example-cpp/.cifuzz-build/llvm_cov" >> cifuzz.yaml
     echo "      path: llvm_cov/$FUZZ_TEST" >> cifuzz.yaml
-    echo "      seeds: cifuzz-corpus/$FUZZ_TEST" >> cifuzz.yaml
+    if [ -d "cifuzz-corpus/$FUZZ_TEST" ]; then
+      echo "      seeds: cifuzz-corpus/$FUZZ_TEST" >> cifuzz.yaml
+    fi
     echo "      max_run_time: 1800" >> cifuzz.yaml
     echo "      parallel_executions: 1" >> cifuzz.yaml
 
@@ -33,14 +40,14 @@ if test -f "$FUZZ_TEST.cpp"; then
     cp ../.cifuzz-build/llvm_cov/$FUZZ_TEST llvm_cov/$FUZZ_TEST
 
     # Add seeds
-    mkdir cifuzz-corpus
-    cp -r ../.cifuzz-corpus/$FUZZ_TEST cifuzz-corpus
+    if [ -d "cifuzz-corpus/$FUZZ_TEST" ]; then
+      mkdir cifuzz-corpus
+      cp -r ../.cifuzz-corpus/$FUZZ_TEST cifuzz-corpus
+    fi
 
     # Repack everything and put it in root directory like nothing happened
     tar cfvz $FUZZ_TEST.tar.gz *
     mv $FUZZ_TEST.tar.gz ..
     cd ..
     rm -r unpacking
-else
-  echo "Usage: $0 fuzz_test_name"
-fi
+done
