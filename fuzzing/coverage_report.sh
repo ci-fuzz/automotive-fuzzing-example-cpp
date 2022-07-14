56e63f014c4943eef2fd0449090f420726905e8d
D. Teuchert <teuchert@code-intelligence.com>
2022-07-14 12:16:31 +0200

    more scripting

--- fuzzing/coverage_report.sh
+++ fuzzing/coverage_report.sh
@@ -6,26 +6,15 @@ FUZZ_TEST=$1
 
 if test -f "$FUZZ_TEST.cpp"; then
 
+  ./fuzzing/create_llvm_target.sh $FUZZ_TEST
+  
   CORPUS_DIR=.cifuzz-corpus/$FUZZ_TEST
-
-  # Build target to measure coverage
-  export CXX=clang++
-  export CC=clang
-  export CFLAGS="-fprofile-instr-generate -fcoverage-mapping"
-  export CXXFLAGS="$CFLAGS"
-  export WORK_DIR=.cifuzz-build/llvm_cov
-  rm -rf $WORK_DIR
-  mkdir -p $WORK_DIR
-  cd $WORK_DIR
-  cmake ../..
-  make
-  clang++ $CXXFLAGS -DCIFUZZ_TEST_NAME=\"$FUZZ_TEST\" -DCIFUZZ_SEED_CORPUS=\".\" -I$HOME/cifuzz/share/cifuzz/include/cifuzz -I../../modules/time_module/src -I../../modules/key_management_module/src -I../../modules/GPS_module/src -I../../modules/crypto_module/src ../../$FUZZ_TEST.cpp ../../fuzzing/StandaloneFuzzTargetMain.cpp libAUTOMOTIVE-FUZZING-EXAMPLE-CPP.a -o $FUZZ_TEST
-
+  cd .cifuzz-build/llvm_cov
+  
   # Run tests with the coverage build
-  echo "$CORPUS_DIR"
   find ../../$CORPUS_DIR -type f -print0 | xargs -0 ./$FUZZ_TEST
   COVDIR=coverage
-  mkdir $COVDIR
+  mkdir -p $COVDIR
   mv default.profraw "$COVDIR"
 
   # Convert raw coverage data
--- fuzzing/fuzz_test/my_fuzz_test_1.cpp
+++ fuzzing/fuzz_test/my_fuzz_test_1.cpp
@@ -5,27 +5,9 @@
 #include <stddef.h>
 
 #include <fuzzer/FuzzedDataProvider.h>
-#include <vector>
 
-#include "GPS_module_1.h"
 #include "crypto_module_1.h"
-#include "crypto_module_2.h"
-#include "key_management_module_1.h"
-#include "time_module_1.h"
 
-// Wrapper function for FuzzedDataProvider.h
-// Writes |num_bytes| of input data to the given destination pointer. If there
-// is not enough data left, writes all remaining bytes and fills the rest with zeros.
-// Return value is the number of bytes written.
-void ConsumeDataAndFillRestWithZeros(void *destination, size_t num_bytes, FuzzedDataProvider *fuzz_data) {
-  if (destination != nullptr) {
-    size_t num_bytes_with_fuzz_data = fuzz_data->ConsumeData(destination, num_bytes);
-    if (num_bytes > num_bytes_with_fuzz_data) {
-      size_t num_bytes_with_zeros = num_bytes - num_bytes_with_fuzz_data;
-      std::memset((char*)destination+num_bytes_with_fuzz_data, 0, num_bytes_with_zeros);
-    }
-  }
-}
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
 
