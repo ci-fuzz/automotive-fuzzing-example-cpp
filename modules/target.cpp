e01b0da04833fa07af6525a41f3a3cf203d99731
Jacek Trossen <jacek.trossen@code-intelligence.com>
2023-05-05 12:35:58 +0200

    base example project

---
+++ modules/target.cpp
@@ -0,0 +1,18 @@
+#include "target.h"
+
+uint8_t targetFunc(const uint8_t * const message, size_t len) {
+    if (len > 4) {
+        if (message[0] == 'F') {
+            if (message[1] == 'U') {            
+                if (message[2] == 'Z') {               
+                    if (message[3] == 'Z') {
+                        int x[3];
+                        int y = 4;
+                        int z = x[y];
+                    }   
+                }   
+            }   
+        }
+    }
+    return 0;
+}
---
+++ modules/target.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+uint8_t targetFunc(const uint8_t * const message, size_t len);
